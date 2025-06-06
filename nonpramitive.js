bc0bbfda5a5a2d966d5b5bc4e243491d64379d25
manishkumar2654 <Hairbymanish@gmail.com>
2025-06-06 10:24:26 +0530

    Add files via upload

---
+++ nonpramitive.js
@@ -0,0 +1,20 @@
+
+const heros = ["shaktiman", "naagraj", "doga"] //arry
+
+ let obj = {
+    name:"manish",
+    age:22,
+    //object => curly bress me key value pair likh te hai
+    // curly bress me jo hai object type data hai
+
+}
+
+
+//function
+// function ko hum ek data type ki tharaha use karege
+const myfun = function(){ 
+    console.log("hello world");
+    
+}  //ye function ka defination hai
+
+console.log(typeof heros);
